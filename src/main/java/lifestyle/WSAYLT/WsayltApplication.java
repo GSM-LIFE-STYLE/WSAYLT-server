0f2a082c435ef0d931044f773443e588916bf267
KimTaeO <taeo0328@gmail.com>
2023-02-11 12:20:05 +0900

    initalizing

---
+++ src/main/java/lifestyle/WSAYLT/WsayltApplication.java
@@ -0,0 +1,13 @@
+package lifestyle.WSAYLT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsayltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsayltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lifestyle/WSAYLT/WsayltApplicationTests.java
@@ -0,0 +1,13 @@
+package lifestyle.WSAYLT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsayltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
