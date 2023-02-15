57dd458b30d0a2edcf6e54eba9ac24d9ef767d0c
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-16 01:01:11 +0900

    Auth 레포지토리 생성

--- src/main/java/lifestyle/WSAYLT/domain/auth/repository/AuthRepository.java
+++ src/main/java/lifestyle/WSAYLT/domain/auth/repository/AuthRepository.java
@@ -1,4 +1,10 @@
 package lifestyle.WSAYLT.domain.auth.repository;
 
-public interface AuthRepository extends {
+import lifestyle.WSAYLT.domain.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuthRepository extends JpaRepository<User, Long> {
+    Optional<User> findByNickname(String nickname);
 }
