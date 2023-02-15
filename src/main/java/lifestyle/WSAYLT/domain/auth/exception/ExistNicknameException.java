0db9cf401d3497602c7726261c5c82994b0d2475
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-16 01:08:48 +0900

    회원가입 예외처리 생성

---
+++ src/main/java/lifestyle/WSAYLT/domain/auth/exception/ExistNicknameException.java
@@ -0,0 +1,7 @@
+package lifestyle.WSAYLT.domain.auth.exception;
+
+public class ExistNicknameException extends Exception {
+    public ExistNicknameException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lifestyle/WSAYLT/domain/auth/exception/NotSamePasswordException.java
@@ -0,0 +1,7 @@
+package lifestyle.WSAYLT.domain.auth.exception;
+
+public class NotSamePasswordException extends Exception {
+    public NotSamePasswordException(String message) {
+        super(message);
+    }
+}
