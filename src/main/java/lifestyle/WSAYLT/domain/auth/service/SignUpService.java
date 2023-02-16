5b9fc225f082baed7ce21e0db5551c3725de1449
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-16 21:39:19 +0900

    @Transactional 어노테이션 추가

--- src/main/java/lifestyle/WSAYLT/domain/auth/service/SignUpService.java
+++ src/main/java/lifestyle/WSAYLT/domain/auth/service/SignUpService.java
@@ -7,6 +7,7 @@ import lifestyle.WSAYLT.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,6 +16,7 @@ public class SignUpService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void execute(SignUpRequest signUpRequest) {
         if (userRepository.existsByNickname(signUpRequest.getNickname())) {
             throw new ExistNicknameException("이미 존재하는 닉네임입니다.");
