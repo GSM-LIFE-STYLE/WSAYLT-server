9a18bd4a3f9e238d0ee57e2a2da1da0a91dcbf30
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-16 21:40:24 +0900

    @EnableWebSecurity 어노테이션 추가 및 문장 추가

--- src/main/java/lifestyle/WSAYLT/global/config/security/SecurityConfig.java
+++ src/main/java/lifestyle/WSAYLT/global/config/security/SecurityConfig.java
@@ -4,14 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -24,6 +27,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 //                .antMatchers("/member/login").permitAll()
                 .antMatchers("/auth").permitAll()
                 .anyRequest().authenticated();
