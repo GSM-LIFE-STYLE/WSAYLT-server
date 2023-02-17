69b01b89a8b71f2c3cba77722c837c30df32543a
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-17 21:11:23 +0900

    :memo: 에러코드 추가

--- src/main/java/lifestyle/WSAYLT/global/error/ErrorCode.java
+++ src/main/java/lifestyle/WSAYLT/global/error/ErrorCode.java
@@ -7,7 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     ALREADY_EXIST_NICKNAME("이미 존재하는 닉네임입니다.", 400),
-    NOT_SAME_PASSWORD("비밀번호가 일치하지 않습니다.", 400);
+    NOT_SAME_PASSWORD("비밀번호가 일치하지 않습니다.", 400),
+    USER_NOT_FOUND("사용자를 찾을 수 없습니다", 404),
+    TOKEN_IS_EXPIRED("토큰이 만료 되었습니다.", 401),
+    TOKEN_NOT_VALID("토큰이 유효 하지 않습니다.", 401),
+    MISMATCH_MEMBER_PASSWORD("비밀번호가 일치하지 않습니다.", 400),
+    REFRESH_TOKEN_NOT_FOUND("존재하지 않는 리프레시 토큰입니다.", 404);
+
 
     private final String message;
     private final int status;
