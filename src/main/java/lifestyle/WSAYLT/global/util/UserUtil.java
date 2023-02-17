adef389e8cd3cdcf6ae080ffb9762680c1c2be66
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-17 21:35:25 +0900

    :memo: UserUtil 작성

---
+++ src/main/java/lifestyle/WSAYLT/global/util/UserUtil.java
@@ -0,0 +1,23 @@
+package lifestyle.WSAYLT.global.util;
+
+import lifestyle.WSAYLT.domain.auth.exception.UserNotFoundException;
+import lifestyle.WSAYLT.domain.user.entity.User;
+import lifestyle.WSAYLT.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class UserUtil {
+
+    private final UserRepository userRepository;
+
+    public User currentUser() {
+        String nickname = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByNickname(nickname)
+                .orElseThrow(() -> new UserNotFoundException("존재하지 않는 사용자입니다"));
+    }
+}
