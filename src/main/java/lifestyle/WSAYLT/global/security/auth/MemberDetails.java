660ea09e63b86e367894b316676991099ac05631
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-17 21:26:23 +0900

    :sparkles: MemberDetails 작성

---
+++ src/main/java/lifestyle/WSAYLT/global/security/auth/MemberDetails.java
@@ -0,0 +1,49 @@
+package lifestyle.WSAYLT.global.security.auth;
+
+import lifestyle.WSAYLT.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class MemberDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
