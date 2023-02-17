a477e5d37c4c669623f478b8fb01ccab208b2b92
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-17 21:28:13 +0900

    :memo: MemberDetailsService 작성

---
+++ src/main/java/lifestyle/WSAYLT/global/security/auth/MemberDetailsService.java
@@ -0,0 +1,23 @@
+package lifestyle.WSAYLT.global.security.auth;
+
+import lifestyle.WSAYLT.domain.user.entity.User;
+import lifestyle.WSAYLT.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
+        User user = userRepository.findByNickname(nickname)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        return new MemberDetails(user);
+    }
+}
