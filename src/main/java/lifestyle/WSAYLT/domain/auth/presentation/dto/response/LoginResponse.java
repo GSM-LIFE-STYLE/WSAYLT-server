6b7ef4264854a6b80fa9dabcd0d7fc254fdc3097
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-17 22:38:19 +0900

    :zap: final 추가

--- src/main/java/lifestyle/WSAYLT/domain/auth/presentation/dto/response/LoginResponse.java
+++ src/main/java/lifestyle/WSAYLT/domain/auth/presentation/dto/response/LoginResponse.java
@@ -10,9 +10,9 @@ import java.time.ZonedDateTime;
 @Builder
 public class LoginResponse {
 
-    private String accessToken;
+    private final String accessToken;
 
-    private String refreshToken;
+    private final String refreshToken;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private ZonedDateTime expiredAt;
