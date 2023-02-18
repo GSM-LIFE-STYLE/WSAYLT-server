a7f9d0ae87abf2004d23f8c781d3ccfdc524f4eb
KimTaeO <taeo0328@gmail.com>
2023-02-19 01:24:26 +0900

    :sparkles: 비밀번호 변경 request 조건 추가 및 불변성 제거

--- src/main/java/lifestyle/WSAYLT/domain/user/presentation/dto/request/ChangePasswordRequest.java
+++ src/main/java/lifestyle/WSAYLT/domain/user/presentation/dto/request/ChangePasswordRequest.java
@@ -3,9 +3,15 @@ package lifestyle.WSAYLT.domain.user.presentation.dto.request;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @RequiredArgsConstructor
 public class ChangePasswordRequest {
-    private final String password;
-    private final String newPassword;
+    @NotBlank(message = "기존 비밀번호를 입력해야 합니다")
+    private String password;
+    @NotBlank(message = "수정할 비밀번호를 입력해야 합니다")
+    @Pattern(regexp = "(?=.*\\W)(?=\\S+$).{8,16}", message = "변경한 비밀번호는 특수문자가 1개 이상 포함되어야 하고 글자 수는 8 ~ 16자 이내로 입력해야 합니다.")
+    private String newPassword;
 }
