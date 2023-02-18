f3aa3c104acf86526d6fa03afed9d2c206ca3ce4
uuuuuuuk <s22034@gsm.hs.kr>
2023-02-18 21:49:48 +0900

    :memo: TokenReissueService 작성

---
+++ src/main/java/lifestyle/WSAYLT/domain/auth/service/TokenReissueService.java
@@ -0,0 +1,49 @@
+package lifestyle.WSAYLT.domain.auth.service;
+
+
+import lifestyle.WSAYLT.domain.auth.entity.RefreshToken;
+import lifestyle.WSAYLT.domain.auth.exception.RefreshTokenNotFoundException;
+import lifestyle.WSAYLT.domain.auth.presentation.dto.response.NewTokenResponse;
+import lifestyle.WSAYLT.domain.auth.repository.RefreshTokenRepository;
+import lifestyle.WSAYLT.global.security.exception.TokenNotValidException;
+import lifestyle.WSAYLT.global.security.jwt.TokenProvider;
+import lifestyle.WSAYLT.global.security.jwt.properties.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.time.ZonedDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class TokenReissueService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenProvider tokenProvider;
+    private final JwtProperties jwtProperties;
+
+    @Transactional(rollbackFor = Exception.class)
+    public NewTokenResponse execute(String reqToken){
+        String email = tokenProvider.getUserEmail(reqToken,jwtProperties.getRefreshSecret());
+        RefreshToken token = refreshTokenRepository.findById(email)
+                .orElseThrow(()->new RefreshTokenNotFoundException("존재하지 않은 refreshToken 입니다."));
+
+        if(!token.getRefreshToken().equals(reqToken)){
+            throw new TokenNotValidException("토큰이 유효하지 않습니다.");
+        }
+
+        String accessToken = tokenProvider.generatedAccessToken(email);
+        String refreshToken = tokenProvider.generatedRefreshToken(email);
+        ZonedDateTime expiredAt = tokenProvider.getExpiredAtToken(accessToken, jwtProperties.getAccessSecret());
+        token.exchangeRefreshToken(refreshToken);
+        refreshTokenRepository.save(token);
+        return NewTokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .expiredAt(expiredAt)
+                .build();
+
+
+    }
+}
