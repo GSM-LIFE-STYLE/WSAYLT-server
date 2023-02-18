337e90ca4adfc27e367b12a432d43d806c60bd2d
Kim gyeong su <sk62628504@gmail.com>
2023-02-18 15:19:33 +0900

    :memo: Board entity 작성

---
+++ src/main/java/lifestyle/WSAYLT/domain/board/entity/Board.java
@@ -0,0 +1,31 @@
+package lifestyle.WSAYLT.domain.board.entity;
+
+
+import lifestyle.WSAYLT.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="board_id", nullable = false)
+    private Long boardId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "url",nullable = false)
+    private String url;
+
+
+}
