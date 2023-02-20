3816a9a32a5482caeb8498d5e65ad6edc5e43b99
Kim gyeong su <sk62628504@gmail.com>
2023-02-20 17:30:02 +0900

    :memo: existsHeartByUserAndBoard 메서드 생성

--- src/main/java/lifestyle/WSAYLT/domain/board/service/HeartInsertService.java
+++ src/main/java/lifestyle/WSAYLT/domain/board/service/HeartInsertService.java
@@ -2,10 +2,12 @@ package lifestyle.WSAYLT.domain.board.service;
 
 import lifestyle.WSAYLT.domain.auth.exception.UserNotFoundException;
 import lifestyle.WSAYLT.domain.board.entity.Board;
+import lifestyle.WSAYLT.domain.board.entity.Heart;
 import lifestyle.WSAYLT.domain.board.exception.AlreadyInsertHeartException;
 import lifestyle.WSAYLT.domain.board.exception.BoardNotFoundException;
 import lifestyle.WSAYLT.domain.board.presentation.dto.request.HeartRequest;
 import lifestyle.WSAYLT.domain.board.repository.BoardRepository;
+import lifestyle.WSAYLT.domain.board.repository.HeartRepository;
 import lifestyle.WSAYLT.domain.user.entity.User;
 import lifestyle.WSAYLT.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,27 +21,29 @@ import org.springframework.transaction.annotation.Transactional;
 
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
+    private final HeartRepository heartRepository;
 
 
 
     @Transactional
     public void execute(Long boardId, HeartRequest heartRequest){
-
-
         User user = userRepository.findByNickname(heartRequest.getNickname())
                 .orElseThrow(() -> new UserNotFoundException("존재하지 않는 유저입니다"));
 
         Board board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new BoardNotFoundException("게시글이 존재하지 않습니다"));
 
-        if(user.getHeartBoard().contains(board)){
+        if(heartRepository.existsHeartByUserAndBoard(user,board)){
             throw new AlreadyInsertHeartException("이미 좋아요를 누르셨습니다");
         }
-        board.setUser(user);
-        user.getHeartBoard().add(board);
+        Heart heart = Heart.builder()
+                .board(board)
+                .user(user)
+                .build();
 
         board.updateHeart(board.getHeartCount()+1);
         boardRepository.save(board);
+        heartRepository.save(heart);
 
     }
 }
